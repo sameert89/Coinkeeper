c2dab48d86b99dba293d440212ea734df55c3f6b
sameert89 <91889637+sameert89@users.noreply.github.com>
2024-04-11 00:58:51 +0530

    fixed theming, added profiles, added date autoload, added persistent theme store

--- src/app/profile/feature/profile-container/profile-container.component.ts
+++ src/app/profile/feature/profile-container/profile-container.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { NavbarComponent } from '../../../dashboard/feature/navbar/navbar.component';
@@ -9,7 +14,7 @@ import { ProfileDataService } from '../../data-access/profile-data.service';
 @Component({
   selector: 'app-profile-container',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ReactiveFormsModule],
   templateUrl: './profile-container.component.html',
   styleUrl: './profile-container.component.scss',
 })
@@ -18,7 +23,15 @@ export class ProfileContainerComponent {
     private profileDataService: ProfileDataService,
     private snackbar: MatSnackBar
   ) {}
-  userData!: UserDataModel;
+  userData: UserDataModel = {
+    name: '',
+    email: '',
+    preferences: {
+      budget: 0,
+      defaultPage: '',
+      customCategories: [],
+    },
+  };
   userDataFormGroup = new FormGroup({
     name: new FormControl(this.userData.name, Validators.required),
     email: new FormControl(this.userData.email, [
@@ -39,31 +52,40 @@ export class ProfileContainerComponent {
   });
   ngOnInit() {
     this.profileDataService.fetchProfileData().subscribe({
-      next: (response) => {},
-      error: (error) => {
-        console.log(error);
-      },
-    });
-  }
-  updateProfileData(): void {
-    const updatedProfileData: UserDataModel = {
-      name: this.userDataFormGroup.value.name!,
-      email: this.userDataFormGroup.value.email!,
-      preferences: {
-        budget: this.userDataFormGroup.value.preferences?.budget!,
-        defaultPage: this.userDataFormGroup.value.preferences?.defaultPage!,
-        customCategories:
-          this.userDataFormGroup.value.preferences?.customCategories!,
-      },
-    };
-    this.profileDataService.updateProfileData(updatedProfileData).subscribe({
       next: (response) => {
-        this.snackbar.open('Userdata updated successfuly', 'Close');
+        this.userDataFormGroup.value.name = response.name;
+        this.userDataFormGroup.value.email = response.email;
+        this.userDataFormGroup.value.preferences!.budget =
+          response.preferences.budget;
+        this.userDataFormGroup.value.preferences!.defaultPage =
+          response.preferences.defaultPage;
+        this.userDataFormGroup.value.preferences!.customCategories =
+          response?.preferences?.customCategories || [];
       },
       error: (error) => {
-        this.snackbar.open('Error while updating userdata', 'Close');
         console.log(error);
       },
     });
   }
+  // updateProfileData(): void {
+  //   const updatedProfileData: UserDataModel = {
+  //     name: this.userDataFormGroup.value.name!,
+  //     email: this.userDataFormGroup.value.email!,
+  //     preferences: {
+  //       budget: this.userDataFormGroup.value.preferences?.budget!,
+  //       defaultPage: this.userDataFormGroup.value.preferences?.defaultPage!,
+  //       customCategories:
+  //         this.userDataFormGroup.value.preferences?.customCategories!,
+  //     },
+  //   };
+  //   this.profileDataService.updateProfileData(updatedProfileData).subscribe({
+  //     next: (response) => {
+  //       this.snackbar.open('Userdata updated successfuly', 'Close');
+  //     },
+  //     error: (error) => {
+  //       this.snackbar.open('Error while updating userdata', 'Close');
+  //       console.log(error);
+  //     },
+  //   });
+  // }
 }
