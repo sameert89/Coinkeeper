8f38d9ef209aabc408fd7892ef5bd509bf0c1b87
X6STRIVE <X6STRIVE@IN05N001NS>
2024-04-16 10:41:13 +0530

    Optimized LCP, added login failure toasts

--- src/app/auth/ui/login-form/login-form.component.ts
+++ src/app/auth/ui/login-form/login-form.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { StartButtonComponent } from '../../../shared/ui/start-button/start-button.component';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { Router, RouterModule } from '@angular/router';
+
+import { StartButtonComponent } from '../../../shared/ui/start-button/start-button.component';
 import { AuthService } from '../../data-access/auth.service';
 import { LoginFormModel } from '../../data-access/login-form.model';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+
 @Component({
   selector: 'app-login-form',
   standalone: true,
@@ -22,12 +20,17 @@ import {
     StartButtonComponent,
     RouterModule,
     ReactiveFormsModule,
+    MatSnackBarModule,
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private snackBar: MatSnackBar
+  ) {}
   title: string = 'Log In';
   email: string = '';
   password: string = '';
@@ -41,7 +44,10 @@ export class LoginFormComponent {
         this.router.navigate(['/dashboard']);
       },
       error: (error) => {
-        alert('Failed to Log In');
+        this.snackBar.open(
+          'Login Failed, Please Check Your Credentials',
+          'Close'
+        );
       },
     });
   }
